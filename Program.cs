9618e37e0c0b13cec5c9aedc7cc9058721594611
Егор Сивцев <gumben7@gmail.com>
2022-12-16 11:51:44 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ThreeTwo {
+    class Program {
+        static void Main(string[] args) {
+            int numSys = System.Int32.Parse(System.Console.ReadLine());
+            string number = System.Console.ReadLine();
+            int Decimal = 0;
+            int k = number.Length - 1;
+            foreach(char c in number) {
+                if (c >= 'a') {
+                    Decimal += (int)System.Math.Pow(numSys, k) * (c - 'a' + 10);
+                }
+                else {
+                    Decimal += (int)System.Math.Pow(numSys, k) * (c - '0');
+                }
+                --k;
+            }
+            System.Console.WriteLine(Decimal);
+        }
+    }
+}
